2ae412e39587edfba5e2d166330b265e2cb5b8f9
Kauan Felipe <kauan_falima@hotmail.com>
2021-12-12 21:14:32 -0300

    fix bugs

--- web/js/login.js
+++ web/js/login.js
@@ -70,38 +70,8 @@ function cadastrar() {
 
   axios.post('http://localhost:8080/cadastrar', {email, password})
       .then(function (res){
-          console.log(res.data)
-          alert("Cadastrado com sucesso");
-    var writer;
-
-    if (email.length < 3) {
-      writer = document.querySelector(".email-validation");
-      writer.innerHTML = "Email inválido! Mínimo de 3 caracteres.";
-      return;
-    }
-    if (senha.length < 3) {
-      writer = document.querySelector(".password-validation");
-      writer.innerHTML = "Senha inválida! Mínimo de 3 caracteres.";
-      return;
-    }
-    if (localStorage.getItem("login") == 1) return;
-
-    axios
-      .post("https://reqres.in/api/login", {
-        email: email,
-        password: senha,
-      })
-      .then(function (response) {
-        if (response.status == 200) {
-          loginSucessful(email);
-        }
-      })
-      .catch(function (error) {
-        writer = document.querySelector(".email-validation");
-        writer.innerHTML = "";
-        writer.innerHTML = "Login inválido!";
-        writer = document.querySelector(".password-validation");
-        writer.innerHTML = "";
+          console.log(res.data);
+          alert("Cadastrado com sucesso!");
       });
 }
 
@@ -112,7 +82,7 @@ function entrar() {
   axios.post('http://localhost:8080/entrar', {email, password})
       .then(function (res){
           loginSucessful(res.data);
-          alert("login funcionou");
+          alert("Usuário logou!");
       });
 }
 
@@ -136,10 +106,9 @@ function logged() {
     temp = document.querySelector(".hide-logged");
     temp.className = temp.className.replace("hide-logged", "logged");
     temp = document.querySelector(".div-logged");
-    temp.innerHTML = "Bem-vindo, " + sessionStorage.getItem("email");
+    temp.innerHTML = "Bem-vindo";
     temp = document.querySelector(".hide-search");
     temp.className = temp.className.replace("hide-search", "search-field");
-<<<<<<< HEAD
   }
 }
 
@@ -188,13 +157,13 @@ function postar(){
   var author = document.getElementById("name").value;
 
   if (!comment || comment.length < 1 || typeof comment === "undefined") {
-      alert("O post deve ter um título/assunto");
+      alert("O comentário deve ser preenchido.");
       return false;
   }
 
   axios.post('http://localhost:8080/postar', {comment, author})
       .then(function (res){
-          alert("Postagem feita");
+          alert("Postagem feita!");
           document.getElementById("post").value = "";
       });
 }
@@ -225,68 +194,3 @@ function buscar(){
 
       });
 }
-    localStorage.setItem("login", 1);
-    localStorage.setItem("email", email);
-  }
-  
-  function logged() {
-    if (localStorage.getItem("login") == 1) {
-      var temp = document.querySelector(".login");
-      temp.className = temp.className.replace("login", "hide-loggin");
-      temp = document.querySelector(".hide-logged");
-      temp.className = temp.className.replace("hide-logged", "logged");
-      temp = document.querySelector(".div-logged");
-      temp.innerHTML = "Bem-vindo, " + localStorage.getItem("email");
-      temp = document.querySelector(".hide-search");
-      temp.className = temp.className.replace("hide-search", "search-field");
-    }
-  }
-  
-  function logout() {
-    localStorage.clear();
-    var temp = document.querySelector(".hide-loggin");
-    temp.className = temp.className.replace("hide-loggin", "login");
-    temp = document.querySelector(".logged");
-    temp.className = temp.className.replace("logged", "hide-logged");
-    temp = document.querySelector(".div-logged");
-    temp.innerHTML = "";
-    temp = document.querySelector(".search-container");
-    temp.innerHTML = "";
-    temp = document.querySelector(".search-field");
-    temp.className = temp.className.replace("search-field", "hide-search");
-    temp = document.querySelector("#email");
-    temp.value = "";
-    temp = document.querySelector("#password");
-    temp.value = "";
-    temp = document.querySelector(".email-validation");
-    temp.innerHTML = "";
-    temp = document.querySelector(".password-validation");
-    temp.innerHTML = "";
-    temp = document.querySelector("#search");
-    temp.value = "";
-  }
-  
-  document.querySelector(".btn-logout").addEventListener("click", function () {
-    logout();
-  });
-  
-  logged();
-
-  var container = document.querySelector('ul');
-  document.querySelector(".btn-search").addEventListener("click", function () {
-    var query = document.querySelector('.search').value;
-    axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`)
-         .then(function (res) {
-           console.log(res);
-           var results = res.data.results;
-           for (var index = 0; index < results.length && index < 10; index++) {
-             var li = document.createElement('li');
-                 img = document.createElement('img');
-                 id_img = results[index].id;
-             li.innerHTML = results[index].name;
-             img.src = 'https://rickandmortyapi.com/api/character/avatar/' + id_img + '.jpeg';
-             li.appendChild(img);
-             container.appendChild(li);
-           }
-         })
-  });
